b528ff84bf6f63d8ece4dca328728489af972fa4
FerGon <ferxeneize22@gmail.com>
2023-06-15 21:10:52 -0300

    clase 20

--- main.js
+++ main.js
@@ -1,8 +1,16 @@
 const nvarEmail = document.querySelector (".navbar-email")
 const cambiarMenu = document.querySelector(".desktop-menu")
+const iconHamburguesaMobile = document.querySelector (".menu")
+const cambiarMenuMobile = document.querySelector(".mobile-menu")
 
 nvarEmail.addEventListener("click", funcionCambiarMenu)
 
 function funcionCambiarMenu () {
     cambiarMenu.classList.toggle("inactiveE");
+}
+
+iconHamburguesaMobile.addEventListener("click", funcionCambiarMenuMobile)
+
+function funcionCambiarMenuMobile () {
+    cambiarMenuMobile.classList.toggle("inactiveE");
 }
