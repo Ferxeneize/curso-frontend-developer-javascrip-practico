24c35c9f612d115d5727bf54b7d2f2088a186e9a
FerGon <ferxeneize22@gmail.com>
2023-06-15 22:03:07 -0300

    Clase 21

--- main.js
+++ main.js
@@ -2,15 +2,24 @@ const nvarEmail = document.querySelector (".navbar-email")
 const cambiarMenu = document.querySelector(".desktop-menu")
 const iconHamburguesaMobile = document.querySelector (".menu")
 const cambiarMenuMobile = document.querySelector(".mobile-menu")
+const iconCarrito = document.querySelector (".navbar-shopping-cart")
+const cambiarMenuCarrito = document.querySelector(".product-detail")
 
-nvarEmail.addEventListener("click", funcionCambiarMenu)
-
+nvarEmail.addEventListener("click", funcionCambiarMenu);
 function funcionCambiarMenu () {
     cambiarMenu.classList.toggle("inactiveE");
+    cambiarMenuCarrito.classList.add("inactiveE")
 }
 
-iconHamburguesaMobile.addEventListener("click", funcionCambiarMenuMobile)
-
+iconHamburguesaMobile.addEventListener("click", funcionCambiarMenuMobile);
 function funcionCambiarMenuMobile () {
     cambiarMenuMobile.classList.toggle("inactiveE");
-}
+    cambiarMenuCarrito.classList.add("inactiveE");
+};
+
+iconCarrito.addEventListener("click", funcionCambiarMenuCarrito);
+function funcionCambiarMenuCarrito () {
+    cambiarMenuCarrito.classList.toggle("inactiveE");
+    cambiarMenu.classList.add("inactiveE");
+    cambiarMenuMobile.classList.add("inactiveE");
+};
